820f7382b4d7eaa21119fd0d158e335277c7399e
Daniel Saunders <dsaunders410@gmail.com>
2017-06-30 15:59:16 -0600

    Add some files to make easier to setup

---
+++ dibco_submission_no_crf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CAFFE_ROOT=/opt/caffe_waldol1
+export PYCAFFE_ROOT=$CAFFE_ROOT/python
+export PYTHONPATH=$PYCAFFE_ROOT:$PYTHONPATH
+export PATH=$CAFFE_ROOT/build/tools:$PYCAFFE_ROOT:$PATH
+
+./dibco_submission_no_crf.py "$@"
+
---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+export CAFFE_ROOT=/opt/caffe_waldol1
+
+apt-get update && apt-get install -y --no-install-recommends \
+        build-essential \
+        cmake \
+        git \
+        wget \
+        libatlas-base-dev \
+        libboost-all-dev \
+        libgflags-dev \
+        libgoogle-glog-dev \
+        libhdf5-serial-dev \
+        libleveldb-dev \
+        liblmdb-dev \
+        libopencv-dev \
+        libprotobuf-dev \
+        libsnappy-dev \
+        protobuf-compiler \
+        python-dev \
+        python-numpy \
+        python-opencv \
+        python-pip \
+        python-scipy \
+        libpng12-dev \
+        libblas-dev \
+        libfreetype6-dev \
+        liblapack-dev \
+        unzip
+
+pip install -U cython
+pip install git+https://github.com/lucasb-eyer/pydensecrf.git
+
+mkdir $CAFFE_ROOT
+cd $CAFFE_ROOT
+
+git clone --depth 1 https://github.com/waldol1/caffe.git .
+for req in $(cat python/requirements.txt) pydot; do
+    pip install $req
+done
+
+mkdir build && cd build && \
+cmake -DCPU_ONLY=1 .. && \
+make -j"$(nproc)"
+
+export PYCAFFE_ROOT=$CAFFE_ROOT/python
+export PYTHONPATH=$PYCAFFE_ROOT:$PYTHONPATH
+export PATH=$CAFFE_ROOT/build/tools:$PYCAFFE_ROOT:$PATH
+export echo "$CAFFE_ROOT/build/lib" >> /etc/ld.so.conf.d/caffe.conf && ldconfig
+
+./get_howe_code.sh
+
